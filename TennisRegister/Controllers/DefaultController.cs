cb2a57617fcf8f7f70ea217a84551fa0026109f2
Barış Bükümcüler <barisbukumculer@gmail.com>
2024-03-22 17:28:03 +0300

    Add project files.

---
+++ TennisRegister/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TennisRegister.Models.Entity;
+
+namespace TennisRegister.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        WebMvcDBEntities db = new WebMvcDBEntities();
+        public ActionResult Index()
+        {
+            List<SelectListItem> values = (from i in db.TBLSEHIR.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.SEHIR,
+                                               Value = i.ID.ToString(),
+                                           }).ToList();
+            ViewBag.Vls = values;
+            return View();
+        }
+    }
+}
